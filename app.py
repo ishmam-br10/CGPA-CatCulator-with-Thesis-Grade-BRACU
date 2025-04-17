5ef06eb3fc99086777d1c185c35fc0039f67dedb
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2025-04-17 19:27:19 +0600

    bug fix

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+@app.route('/calculate', methods=['POST'])
 def calculate_cgpa():
     if request.method == 'POST':
         try:
